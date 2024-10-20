7bf72ea96dcb7a0cff414795e271ff870689f7a0
harry lachenmayer <harrylachenmayer@gmail.com>
2024-10-20 16:55:17 +0100

    fahrenheit/celsius

--- Sources/Propagators/Propagators.swift
+++ Sources/Propagators/Propagators.swift
@@ -3,10 +3,12 @@
 import Collections
 import Foundation
 
-typealias Job = @Sendable () async -> Void
+public actor Scheduler {
+  public typealias Job = @Sendable () async -> Void
 
-actor Scheduler {
   private var jobs = Deque<Job>()
+
+  public init() {}
 
   func schedule(_ job: @escaping Job) {
     jobs.append(job)
@@ -35,47 +37,45 @@ actor Scheduler {
   }
 }
 
-let scheduler = Scheduler()
+public actor Propagator {
+  let alert: Scheduler.Job
 
-actor Propagator {
-  static func make<Output: Sendable>(_ f: @escaping @Sendable () -> Output, output: Cell<Output>)
+  init(_ alert: @escaping Scheduler.Job) {
+    self.alert = alert
+  }
+}
+
+extension Propagator: Equatable, Hashable {
+  nonisolated var id: ObjectIdentifier { ObjectIdentifier(self) }
+
+  public static func == (lhs: Propagator, rhs: Propagator) -> Bool {
+    lhs.id == rhs.id
+  }
+
+  public nonisolated func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
+  }
+}
+
+public struct PropagationNetwork {
+  private let scheduler: Scheduler
+
+  public init(scheduler: Scheduler = Scheduler()) {
+    self.scheduler = scheduler
+  }
+
+  public func run() async {
+    await scheduler.run()
+  }
+
+  public func cell<Value>(_ initialValue: Value? = nil) -> Cell<Value>
+  where Value: Equatable, Value: Sendable {
+    Cell(scheduler: scheduler, content: initialValue)
+  }
+
+  public func propagator(neighbors: [AnyCell], alert: @escaping Scheduler.Job)
     async
   {
-    await Propagator.make(neighbors: []) {
-      let o = f()
-      await output.addContent(o)
-    }
-  }
-
-  static func make<Input: Sendable, Output: Sendable>(
-    _ f: @escaping @Sendable (Input) -> Output,
-    _ input: Cell<Input>,
-    output: Cell<Output>
-  ) async {
-    await Propagator.make(neighbors: [input.erase()]) {
-      let i = await input.content
-      let o = i.map { f($0) }
-      await output.addContent(o)
-    }
-  }
-
-  static func make<I1: Sendable, I2: Sendable, Output: Sendable>(
-    _ f: @escaping @Sendable (I1, I2) -> Output,
-    _ input1: Cell<I1>,
-    _ input2: Cell<I2>,
-    output: Cell<Output>
-  ) async {
-    await Propagator.make(neighbors: [input1.erase(), input2.erase()]) {
-      async let i1 = input1.content
-      async let i2 = input2.content
-      let (i1_, i2_) = await (i1, i2)
-      guard let i1_, let i2_ else { return }
-      let o = f(i1_, i2_)
-      await output.addContent(o)
-    }
-  }
-
-  static func make(neighbors: [AnyCell], alert: @escaping Job) async {
     let propagator = Propagator(alert)
     for neighbor in neighbors {
       await scheduler.schedule {
@@ -85,74 +85,89 @@ actor Propagator {
     await scheduler.alert(propagator: propagator)
   }
 
-  let alert: Job
+  public func propagator<Output: Sendable>(
+    _ f: @escaping @Sendable () -> Output, output: Cell<Output>
+  ) async {
+    await propagator(neighbors: []) {
+      let o = f()
+      await output.addContent(o)
+    }
+  }
 
-  private init(_ alert: @escaping Job) {
-    self.alert = alert
+  public func propagator<Input: Sendable, Output: Sendable>(
+    _ f: @escaping @Sendable (Input) -> Output,
+    _ input: Cell<Input>,
+    output: Cell<Output>
+  ) async {
+    await propagator(neighbors: [input.erase()]) {
+      let i = await input.content
+      let o = i.map { f($0) }
+      await output.addContent(o)
+    }
+  }
+
+  public func propagator<I1: Sendable, I2: Sendable, Output: Sendable>(
+    _ f: @escaping @Sendable (I1, I2) -> Output,
+    _ input1: Cell<I1>,
+    _ input2: Cell<I2>,
+    output: Cell<Output>
+  ) async {
+    await propagator(neighbors: [input1.erase(), input2.erase()]) {
+      async let i1 = input1.content
+      async let i2 = input2.content
+      let (i1_, i2_) = await (i1, i2)
+      guard let i1_, let i2_ else { return }
+      let o = f(i1_, i2_)
+      await output.addContent(o)
+    }
+  }
+
+  public func constant<Value: Sendable>(_ value: Value, cell: Cell<Value>) async {
+    await propagator({ value }, output: cell)
+  }
+
+  // Math
+
+  public func add(lhs: Cell<Double>, rhs: Cell<Double>, output: Cell<Double>) async {
+    await propagator({ lhs, rhs in lhs + rhs }, lhs, rhs, output: output)
+  }
+
+  public func subtract(lhs: Cell<Double>, rhs: Cell<Double>, output: Cell<Double>) async {
+    await propagator({ lhs, rhs in lhs - rhs }, lhs, rhs, output: output)
+  }
+
+  public func multiply(lhs: Cell<Double>, rhs: Cell<Double>, output: Cell<Double>) async {
+    await propagator({ lhs, rhs in lhs * rhs }, lhs, rhs, output: output)
+  }
+
+  public func divide(lhs: Cell<Double>, rhs: Cell<Double>, output: Cell<Double>) async {
+    await propagator({ lhs, rhs in lhs / rhs }, lhs, rhs, output: output)
+  }
+
+  public func sum(lhs: Cell<Double>, rhs: Cell<Double>, total: Cell<Double>) async {
+    await add(lhs: lhs, rhs: rhs, output: total)
+    await subtract(lhs: total, rhs: lhs, output: rhs)
+    await subtract(lhs: total, rhs: rhs, output: lhs)
+  }
+
+  public func product(lhs: Cell<Double>, rhs: Cell<Double>, total: Cell<Double>) async {
+    await multiply(lhs: lhs, rhs: rhs, output: total)
+    await divide(lhs: total, rhs: lhs, output: rhs)
+    await divide(lhs: total, rhs: rhs, output: lhs)
   }
 }
 
-extension Propagator {
-  static func constant<Value: Sendable>(_ value: Value, cell: Cell<Value>) async {
-    await Propagator.make({ value }, output: cell)
-  }
-
-  static func add(lhs: Cell<Double>, rhs: Cell<Double>, output: Cell<Double>) async {
-    await Propagator.make({ lhs, rhs in lhs + rhs }, lhs, rhs, output: output)
-  }
-
-  static func subtract(lhs: Cell<Double>, rhs: Cell<Double>, output: Cell<Double>) async {
-    await Propagator.make({ lhs, rhs in lhs - rhs }, lhs, rhs, output: output)
-  }
-
-  static func multiply(lhs: Cell<Double>, rhs: Cell<Double>, output: Cell<Double>) async {
-    await Propagator.make({ lhs, rhs in lhs * rhs }, lhs, rhs, output: output)
-  }
-
-  static func divide(lhs: Cell<Double>, rhs: Cell<Double>, output: Cell<Double>) async {
-    await Propagator.make({ lhs, rhs in lhs / rhs }, lhs, rhs, output: output)
-  }
-}
-
-extension Propagator: Equatable, Hashable {
-  nonisolated var id: ObjectIdentifier { ObjectIdentifier(self) }
-
-  static func == (lhs: Propagator, rhs: Propagator) -> Bool {
-    lhs.id == rhs.id
-  }
-
-  nonisolated func hash(into hasher: inout Hasher) {
-    hasher.combine(id)
-  }
-}
-
-struct AnyCell: Sendable {
-  private let addNeighbor_: @Sendable (Propagator) async -> Void
-
-  init<Content: Equatable>(_ cell: Cell<Content>) {
-    addNeighbor_ = { propagator in await cell.addNeighbor(propagator) }
-  }
-
-  func addNeighbor(_ propagator: Propagator) async {
-    await addNeighbor_(propagator)
-  }
-}
-
-extension Cell {
-  nonisolated func erase() -> AnyCell {
-    AnyCell(self)
-  }
-}
-
-actor Cell<Content: Equatable> {
-  private(set) var content: Content?
+public actor Cell<Content: Equatable> {
+  private let scheduler: Scheduler
+  public private(set) var content: Content?
   private var neighbors: Set<Propagator> = []
 
-  init(_ content: Content? = nil) {
+  init(scheduler: Scheduler, content: Content? = nil) {
+    self.scheduler = scheduler
     self.content = content
   }
 
-  func addContent(_ newContent: Content?) async -> Result<(), Inconsistency> {
+  public func addContent(_ newContent: Content?) async -> Result<(), Inconsistency> {
     guard let newContent else {
       return Result.success(())
     }
@@ -173,6 +188,24 @@ actor Cell<Content: Equatable> {
   }
 }
 
-struct Inconsistency: Error {
+public struct Inconsistency: Error {
   let message: String
+}
+
+public struct AnyCell: Sendable {
+  private let addNeighbor_: @Sendable (Propagator) async -> Void
+
+  public init<Content: Equatable>(_ cell: Cell<Content>) {
+    addNeighbor_ = { propagator in await cell.addNeighbor(propagator) }
+  }
+
+  public func addNeighbor(_ propagator: Propagator) async {
+    await addNeighbor_(propagator)
+  }
+}
+
+extension Cell {
+  public nonisolated func erase() -> AnyCell {
+    AnyCell(self)
+  }
 }
--- Tests/PropagatorsTests/PropagatorsTests.swift
+++ Tests/PropagatorsTests/PropagatorsTests.swift
@@ -1,11 +1,45 @@
+import Propagators
 import Testing
-@testable import Propagators
 
 @Test func add() async throws {
-  let lhs = Cell(2.0)
-  let rhs = Cell(3.0)
-  let output = Cell<Double>()
-  await Propagator.add(lhs: lhs, rhs: rhs, output: output)
-  await scheduler.run()
+  let network = PropagationNetwork()
+  let lhs = network.cell(2.0)
+  let rhs = network.cell(3.0)
+  let output: Cell<Double> = network.cell()
+  await network.add(lhs: lhs, rhs: rhs, output: output)
+  await network.run()
   #expect(await output.content == 5.0)
+}
+
+@Test func sum() async throws {
+  let network = PropagationNetwork()
+  let x: Cell<Double> = network.cell()
+  let y = network.cell(3.0)
+  let total = network.cell(5.0)
+  await network.sum(lhs: x, rhs: y, total: total)
+  await network.run()
+  #expect(await x.content == 2.0)
+}
+
+@Test func fahrenheitCelsius() async throws {
+  let network = PropagationNetwork()
+  let c = network.cell(25.0)
+  let f: Cell<Double> = network.cell()
+  await network.fahrenheitCelsius(f: f, c: c)
+  await network.run()
+  let result = await f.content
+  #expect(result == 77.0)
+}
+
+private extension PropagationNetwork {
+  func fahrenheitCelsius(f: Cell<Double>, c: Cell<Double>) async {
+    let thirtyTwo = self.cell(32.0)
+    let five = self.cell(5.0)
+    let nine = self.cell(9.0)
+    let fMinus32: Cell<Double> = self.cell()
+    let cBy9: Cell<Double> = self.cell()
+    await self.sum(lhs: thirtyTwo, rhs: fMinus32, total: f)
+    await self.product(lhs: fMinus32, rhs: five, total: cBy9)
+    await self.product(lhs: c, rhs: nine, total: cBy9)
+  }
 }
